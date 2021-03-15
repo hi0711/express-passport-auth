bf88884f4c7516a070317ac4630b3defbad05d80
hiroshi_sato <h.sato@diamondhead.tech>
2021-03-15 15:04:29 +0900

    feat: 🎸 モジュール・サーバ・テンプレートエンジンの設定
    ✅ Closes: #3

---
+++ index.js
@@ -0,0 +1,21 @@
+// dotenv読み込み
+require('dotenv').config();
+
+// 必要なモジュール
+const express = require('express');
+const app = express();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const path = require('path');
+
+// テンプレートエンジンの指定
+app.set('view engine', 'ejs');
+
+// server
+const PORT = process.env.PORT_NO || 80;
+app.listen(PORT, () => {
+    console.info('listen: ', PORT)
+});
+
