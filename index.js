00836774a732d90f4de128ff288047728c3f4445
hiroshi_sato <h.sato@diamondhead.tech>
2021-03-16 15:20:34 +0900

    fix: 🐛 passportのイニシャライズ処理修正
    ✅ Closes: #9

--- index.js
+++ index.js
@@ -21,19 +21,6 @@ app.use(express.urlencoded({extended: false}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// Router
-const indexRouter = require('./routes/index');
-// const usersRouter = require('./routes/users');
-
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-
-// server
-const PORT = process.env.PORT_NO || 80;
-app.listen(PORT, () => {
-    console.info('listen: ', PORT)
-});
-
 // sessionの設定
 var sess = {
     secret: 'wedding party',
@@ -51,6 +38,19 @@ app.use(session(sess));
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Router
+const indexRouter = require('./routes/index');
+// const usersRouter = require('./routes/users');
+
+app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// server
+const PORT = process.env.PORT_NO || 80;
+app.listen(PORT, () => {
+    console.info('listen: ', PORT)
+});
+
 // secretの定義
 // const SECRET = bcrypt.hashSync(process.env.DEFAULT_SECRET, 10),
 //     SECRET_COMPARE = bcrypt.compareSync(password, SECRET);
@@ -65,7 +65,7 @@ passport.use(new LocalStrategy(
                 return done(null, false, {
                     message: 'User name is incorrect!'
                 });
-            } else if (password !== result[0].password) {
+            } else if (password !== SECRET) {
                 return done(null, false, {
                     message: 'Password is incorrect!'
                 });
