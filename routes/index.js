92123bc6f3d2ee2ad80ef33b61dff8c7e92eaa3d
hiroshi_sato <h.sato@diamondhead.tech>
2021-03-16 10:17:06 +0900

    feat(機能): 🎸 passportを通したルーティングを実装
    ✅ Closes: #9

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 // indexのルーティング
 router.get('/', function(req, res) {
     res.render('login')
 });
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/users',
+    failureRedirect: '/',
+    session: true
+}))
 
 module.exports = router;
 
