f1bfc2185efa3559205b87cfa4324718b0b5065f
hi0711 <admin@hi0711.xyz>
2021-03-28 01:40:40 +0900

    feat(機能): 🎸 api取得実装
    ✅ Closes: #22

--- src_client/fetch-group.js
+++ src_client/fetch-group.js
@@ -3,7 +3,24 @@ export default function fetchGroup() {
     const url = new URL(window.location.href);
     // URLSearchParamsオブジェクト取得
     const params = url.searchParams;
+    // ターゲットになる要素
+    const targetElement = document.querySelector('group');
+    // API問い合わせ先URL
+    const apiURL = 'https://script.google.com/macros/s/AKfycbx8F3rUeX9PEEAQSy-U_m8ICNeeEzdESweGP4lRTGWILOLx0j3GD6r3TbmjTVqEX7CB/exec?q=' + params.get('name');
 
-    console.log(params.get('name'));
+    function returnData() {
+        return fetch(apiURL)
+            .then((response) => response.json())
+            .then((data) => {
+                const userData = [];
+                for (let i in data) {
+                    userData.push(data[i].group)
+                }
+                return userData
+            });
+    }
+
+    const outputData = returnData();
+    targetElement.innerHTML = outputData;
 }
 
